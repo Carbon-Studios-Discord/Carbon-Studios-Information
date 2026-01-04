c1f70b9235fc2740632dac16bc85ce08671cabce
Carbon-Studios-Discord <metica.deronanthony@gmail.com>
2026-01-04 17:15:21 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# This downloads Chrome so your bot can actually browse the web
+# --- 1. DOWNLOAD CHROME ---
 STORAGE_DIR=/opt/render/project/.render
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
@@ -14,4 +14,16 @@ else
   echo "...Using Chrome from cache"
 fi
 
-pip install -r requirements.txt
+# --- 2. GO BACK TO PROJECT ROOT ---
+# This is the important part! We need to go back to where requirements.txt is.
+cd $HOME/project/src || cd /opt/render/project/src
+
+# --- 3. INSTALL REQUIREMENTS ---
+# Use 'pip install' on the file in the current directory
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+else
+    echo "ERROR: requirements.txt not found in $(pwd)"
+    ls -R # This will print all files to your log so you can see where it is
+    exit 1
+fi
